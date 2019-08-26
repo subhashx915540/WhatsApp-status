14784a0712b6f3af772f8d6c5c7407ae4d3deffc
subhash kumar <subhash91550@gmail.com>
2019-08-26 19:47:44 +0530

    whatsapp status saver

---
+++ app/src/main/java/com/example/subhash/statussaver/Utils/Constants.java
@@ -0,0 +1,7 @@
+package com.example.subhash.statussaver.Utils;
+
+public class Constants {
+    public static final String FOLDER_NAME ="/WhatsApp/";
+    public static String MyPREFERENCES = "PREFS";
+    public static final String SAVE_FOLDER_NAME ="/Status Saver/";
+}
